5f79aff958e8a60bb25da165003c9bdc6dbe4b2a
Sadat40 <sahmed40@stuy.edu>
2023-07-31 12:32:12 -0400

    done

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 // Add your code to this file
+let bodyRef= document.querySelector("body");
+bodyRef.style.backgroundColor="DarkBlue";
+
+let h1Ref=document.querySelector("h1");
+h1Ref.style.textDecoration="underline";
+h1Ref.style.fontSize="30px";
+
+let listRef= document.getElementsByClassName("pokemon_names");
+for (let i=0;i<listRef.length;i++){   
+    listRef[i].style["color"]="yellow";
+}
+let images= document.querySelectorAll("img")
+
+for (let i=0;i<images.length;i++){   
+    images[i].style["border-style"]="dotted";
+    images[i].style["border-color"]="yellow";
+}
+
+let footRef= document.querySelector("footer");
+footRef.innerHTML="ALL STAR CODE - DOM PROJECT";
+
